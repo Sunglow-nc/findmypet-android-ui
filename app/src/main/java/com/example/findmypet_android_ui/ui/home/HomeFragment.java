41768719daf677ee7a1b2f54bd0616840edd6b8d
AbiPetheram <abi.gill97@gmail.com>
2024-09-27 12:45:00 +0100

    feat: implement map view button in HomeFragment to go from list to map view

--- app/src/main/java/com/example/findmypet_android_ui/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/home/HomeFragment.java
@@ -1,20 +1,32 @@
 package com.example.findmypet_android_ui.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.findmypet_android_ui.MainActivity;
+import com.example.findmypet_android_ui.R;
 import com.example.findmypet_android_ui.databinding.FragmentHomeBinding;
+import com.example.findmypet_android_ui.ui.maps.MapsFragment;
+import com.google.android.gms.maps.MapFragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private FragmentHomeBinding binding;
+    private View view;
+    private Button mapViewButton;
+    private NavController navController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,16 +34,21 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        view = binding.getRoot();
+        mapViewButton = view.findViewById(R.id.mapview_button);
+        mapViewButton.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        navController = Navigation.findNavController(view);
+        navController.navigate(R.id.action_listView_to_mapview);
     }
 }
