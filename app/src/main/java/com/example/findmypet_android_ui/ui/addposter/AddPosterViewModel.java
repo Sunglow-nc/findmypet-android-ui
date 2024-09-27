c1597e7d72f4b2222fb23830e9783782c204b845
AbiPetheram <abi.gill97@gmail.com>
2024-09-27 16:59:54 +0100

    refactor: rename gallery to add poster

--- app/src/main/java/com/example/findmypet_android_ui/ui/addposter/AddPosterViewModel.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/addposter/AddPosterViewModel.java
@@ -1,14 +1,14 @@
-package com.example.findmypet_android_ui.ui.gallery;
+package com.example.findmypet_android_ui.ui.addposter;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class AddPosterViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public AddPosterViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
