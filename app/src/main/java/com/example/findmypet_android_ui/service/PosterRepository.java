c17d631a61cd9499a3a521947f84bf681b3d4e83
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 12:06:06 +0100

    feat: add properties and constructor to PosterRepository

--- app/src/main/java/com/example/findmypet_android_ui/service/PosterRepository.java
+++ app/src/main/java/com/example/findmypet_android_ui/service/PosterRepository.java
@@ -1,4 +1,19 @@
 package com.example.findmypet_android_ui.service;
 
+import android.app.Application;
+import android.media.tv.interactive.AppLinkInfo;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.findmypet_android_ui.model.Poster;
+
+import java.util.List;
+
 public class PosterRepository {
+    private MutableLiveData<List<Poster>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public PosterRepository(Application application){
+        this.application = application;
+    }
 }
