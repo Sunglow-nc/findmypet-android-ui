422141da7428403ec37ad801d8816ac137888b93
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 13:54:34 +0100

    feat: add properties and constructor to PosterAdapter

--- app/src/main/java/com/example/findmypet_android_ui/ui/home/PosterAdapter.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/home/PosterAdapter.java
@@ -1,12 +1,26 @@
 package com.example.findmypet_android_ui.ui.home;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.findmypet_android_ui.model.Poster;
+
+import java.util.List;
+
 public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterViewHolder> {
+    List<Poster> posterList;
+    Context context;
+    private final RecyclerViewInterface recyclerViewInterface;
+
+    public PosterAdapter(RecyclerViewInterface recyclerViewInterface, Context context, List<Poster> posterList) {
+        this.recyclerViewInterface = recyclerViewInterface;
+        this.context = context;
+        this.posterList = posterList;
+    }
 
     @NonNull
     @Override
