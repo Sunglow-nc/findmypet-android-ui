4d2e65bf8691b06f8f5cc08f9775435b1093056e
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 13:52:35 +0100

    feat: add PosterAdapter class to extend RecyclerView.Adapter with method placeholders

---
+++ app/src/main/java/com/example/findmypet_android_ui/ui/home/PosterAdapter.java
@@ -0,0 +1,30 @@
+package com.example.findmypet_android_ui.ui.home;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.PosterViewHolder> {
+
+    @NonNull
+    @Override
+    public PosterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PosterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class PosterViewHolder extends RecyclerView.ViewHolder{
+
+    }
+}
