20304bb929845838d09036ee95b6655725ebfe2a
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 11:46:54 +0100

    feat: implement getService method

--- app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
+++ app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
@@ -1,9 +1,27 @@
 package com.example.findmypet_android_ui.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
     private static final String BASE_URL = "${BASE_URL}";
 
+    public static PosterApiService getService(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+    }
 }
