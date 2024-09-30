eeeb45a60def2ff2cd7aee2691692b22560dee1c
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 12:40:35 +0100

    fix: update BASE_URL

--- app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
+++ app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "${BASE_URL}";
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
 
     public static PosterApiService getService(){
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
