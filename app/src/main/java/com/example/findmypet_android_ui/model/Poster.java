35b5799bba023c5f1ce8d787057b3b7628ca188e
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 11:23:44 +0100

    feat: implement Poster class with properties and extending BaseObservable

---
+++ app/src/main/java/com/example/findmypet_android_ui/model/Poster.java
@@ -0,0 +1,79 @@
+package com.example.findmypet_android_ui.model;
+
+import android.os.Parcelable;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.findmypet_android_ui.BR;
+
+import java.time.LocalDateTime;
+
+public class Poster extends BaseObservable {
+    private long id;
+    private String datePosted;
+    private String description;
+    private String title;
+    private Pet pet;
+
+    public Poster(long id, String datePosted, String description, String title, Pet pet) {
+        this.id = id;
+        this.datePosted = datePosted;
+        this.description = description;
+        this.title = title;
+        this.pet = pet;
+    }
+
+    public Poster() {
+    }
+
+    @Bindable
+    public long getId() {
+        return id;
+    }
+
+    @Bindable
+    public String getDatePosted() {
+        return datePosted;
+    }
+
+    @Bindable
+    public String getDescription() {
+        return description;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    @Bindable
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    public void setDatePosted(String datePosted) {
+        this.datePosted = datePosted;
+        notifyPropertyChanged(BR.id);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        notifyPropertyChanged(BR.id);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.id);
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+        notifyPropertyChanged(BR.id);
+    }
+}
