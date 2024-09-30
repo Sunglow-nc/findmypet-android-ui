6bd068f6f6d889b094145464066c8719aa6087b6
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 11:43:52 +0100

    feat: create RetrofitInstance class

---
+++ app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
@@ -0,0 +1,9 @@
+package com.example.findmypet_android_ui.service;
+
+import retrofit2.Retrofit;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "${BASE_URL}";
+
+}
