9323e28679ded7ccb463d392f77980a733c228cc
AbiPetheram <abi.gill97@gmail.com>
2024-09-30 15:45:33 +0100

    feat: implement getAllPosters method in MapsViewModel

--- app/src/main/java/com/example/findmypet_android_ui/ui/maps/MapsViewModel.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/maps/MapsViewModel.java
@@ -4,8 +4,12 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
+import com.example.findmypet_android_ui.model.Poster;
 import com.example.findmypet_android_ui.service.PosterRepository;
+
+import java.util.List;
 
 public class MapsViewModel extends AndroidViewModel {
     PosterRepository posterRepository;
@@ -13,5 +17,9 @@ public class MapsViewModel extends AndroidViewModel {
     public MapsViewModel(@NonNull Application application) {
         super(application);
         this.posterRepository = new PosterRepository(application);
+    }
+
+    public LiveData<List<Poster>> getAllPosters(){
+        return posterRepository.getMutableLiveData();
     }
 }
