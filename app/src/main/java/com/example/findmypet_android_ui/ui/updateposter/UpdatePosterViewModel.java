16f1bfce716cff104082a3575b1e9d26bc53874a
AbiPetheram <abi.gill97@gmail.com>
2024-10-02 16:27:07 +0100

    feat: implement updatePoster method in the UpdatePosterViewModel

--- app/src/main/java/com/example/findmypet_android_ui/ui/updateposter/UpdatePosterViewModel.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/updateposter/UpdatePosterViewModel.java
@@ -2,6 +2,13 @@ package com.example.findmypet_android_ui.ui.updateposter;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.findmypet_android_ui.model.Poster;
+import com.example.findmypet_android_ui.service.PosterRepository;
+
 public class UpdatePosterViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    PosterRepository posterRepository;
+
+    public void updatePoster(Long id, Poster poster){
+        posterRepository.updatePoster(id, poster);
+    }
 }
