c8b4753bc76cf710d139e78af81671a71030d2b9
AbiPetheram <abi.gill97@gmail.com>
2024-10-02 15:58:13 +0100

    feat: add UpdatePosterFragment and ViewModel

---
+++ app/src/main/java/com/example/findmypet_android_ui/ui/updateposter/UpdatePosterViewModel.java
@@ -0,0 +1,7 @@
+package com.example.findmypet_android_ui.ui.updateposter;
+
+import androidx.lifecycle.ViewModel;
+
+public class UpdatePosterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
