7cb636e38275279e5d935e49cd9f46ced81093e7
AbiPetheram <abi.gill97@gmail.com>
2024-10-03 10:34:29 +0100

    feat: implement Parceleable in Poster

--- app/src/main/java/com/example/findmypet_android_ui/model/Poster.java
+++ app/src/main/java/com/example/findmypet_android_ui/model/Poster.java
@@ -1,7 +1,9 @@
 package com.example.findmypet_android_ui.model;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 
@@ -10,7 +12,7 @@ import com.example.findmypet_android_ui.BR;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Poster extends BaseObservable {
+public class Poster extends BaseObservable implements Parcelable {
     private Long id;
     private String datePosted;
     private String description;
@@ -27,6 +29,30 @@ public class Poster extends BaseObservable {
 
     public Poster() {
     }
+
+    protected Poster(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        datePosted = in.readString();
+        description = in.readString();
+        title = in.readString();
+        pet = in.readParcelable(Pet.class.getClassLoader());
+    }
+
+    public static final Creator<Poster> CREATOR = new Creator<Poster>() {
+        @Override
+        public Poster createFromParcel(Parcel in) {
+            return new Poster(in);
+        }
+
+        @Override
+        public Poster[] newArray(int size) {
+            return new Poster[size];
+        }
+    };
 
     @Bindable
     public Long getId() {
@@ -76,5 +102,24 @@ public class Poster extends BaseObservable {
     public void setPet(Pet pet) {
         this.pet = pet;
         notifyPropertyChanged(BR.id);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        if (id == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(id);
+        }
+        parcel.writeString(datePosted);
+        parcel.writeString(description);
+        parcel.writeString(title);
+        parcel.writeParcelable(pet, i);
     }
 }
