9bee262116b15548956188e8a37f81a1c72e6909
AbiPetheram <abi.gill97@gmail.com>
2024-10-03 14:12:19 +0100

    feat: add text for about us description

--- app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsFragment.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsFragment.java
@@ -25,7 +25,7 @@ public class AboutUsFragment extends Fragment {
         binding = FragmentAboutusBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
+        final TextView textView = binding.textAboutus;
         aboutUsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsViewModel.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsViewModel.java
@@ -10,7 +10,17 @@ public class AboutUsViewModel extends ViewModel {
 
     public AboutUsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is aboutus fragment");
+        mText.setValue("Welcome to PetFinder, your go-to platform for reuniting lost pets with " +
+                "their families. We understand the deep emotional bond between pet owners and their " +
+                "furry companions, and we know how distressing it can be when a pet goes missing. " +
+                "That’s why we’ve created an easy-to-use, online poster system that helps you share " +
+                "vital information about your lost pet with your local community and beyond.\n" +
+                        "\nOur mission is simple: to connect people and pets, quickly and effectively. " +
+                        "Through PetFinder, you can create detailed, location-based posters for your lost " +
+                        "pets, including photos, descriptions, and last known locations. We provide " +
+                        "a platform where other animal lovers and good Samaritans can view, share, " +
+                        "and respond to your posts, increasing the chances of bringing your beloved " +
+                        "pet home.");
     }
 
     public LiveData<String> getText() {
