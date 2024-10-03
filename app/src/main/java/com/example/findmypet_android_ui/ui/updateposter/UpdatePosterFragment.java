8c9f1f86a829fa2d5c6f0fa4deb0dc50d81ba122
AbiPetheram <abi.gill97@gmail.com>
2024-10-03 11:05:54 +0100

    feat: implement handing of parcelable bundle to get poster

--- app/src/main/java/com/example/findmypet_android_ui/ui/updateposter/UpdatePosterFragment.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/updateposter/UpdatePosterFragment.java
@@ -62,9 +62,13 @@ public class UpdatePosterFragment extends Fragment implements OnMapReadyCallback
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
-        poster = new Poster();
-        pet = new Pet();
-        owner = new Owner();
+
+        //need to create a Bundle and set the poster: bundle.putParcelable("poster", poster);
+        // then  NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        // navController.navigate(R.id.nav_add_poster, bundle);
+        poster = UpdatePosterFragmentArgs.fromBundle(getArguments()).getPoster();
+        pet = poster.getPet();
+        owner = pet.getOwner();
 
         binding.setPoster(poster);
         binding.setPet(pet);
