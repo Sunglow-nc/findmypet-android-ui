95fd5d190315bd97a0a2bb66793225cc832f1a4a
AbiPetheram <abi.gill97@gmail.com>
2024-10-04 12:27:04 +0100

    feat: implement binding between the ViewModel and layout in AboutUsFragment

--- app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsFragment.java
+++ app/src/main/java/com/example/findmypet_android_ui/ui/aboutus/AboutUsFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.findmypet_android_ui.databinding.FragmentAboutusBinding;
-import com.example.findmypet_android_ui.databinding.FragmentAboutusBinding;
 
 public class AboutUsFragment extends Fragment {
 
@@ -25,8 +24,21 @@ public class AboutUsFragment extends Fragment {
         binding = FragmentAboutusBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textAboutus;
-        aboutUsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textInfo = binding.textAboutus;
+        aboutUsViewModel.getText().observe(getViewLifecycleOwner(), textInfo::setText);
+
+        final TextView header = binding.textHeader;
+        aboutUsViewModel.getHeaderText().observe(getViewLifecycleOwner(), header::setText);
+
+        final TextView headerWhoAreWe = binding.headerWhoWeAre;
+        aboutUsViewModel.getDeveloperHeader().observe(getViewLifecycleOwner(), headerWhoAreWe::setText);
+
+        final TextView textWhoAreWe = binding.textWhoWeAre;
+        aboutUsViewModel.getDeveloperDetail().observe(getViewLifecycleOwner(), textWhoAreWe::setText);
+
+        final TextView developerNames = binding.developerNames;
+        aboutUsViewModel.getDeveloperNames().observe(getViewLifecycleOwner(), developerNames::setText);
+
         return root;
     }
 
