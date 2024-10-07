f46aa2ffb42f1609c1960b8459b8c9f5d79e3414
qiuqiu <qiujuanwang87@gmail.com>
2024-10-07 10:16:53 +0100

    minor: add & comment out base url of "http://10.0.2.2:8080/" in RetrofitInstance

--- app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
+++ app/src/main/java/com/example/findmypet_android_ui/service/RetrofitInstance.java
@@ -8,6 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
     private static final String BASE_URL = "http://172.28.86.55:8080/";
+    //private static final String BASE_URL = "http://10.0.2.2:8080/";
 
     public static PosterApiService getService(){
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
